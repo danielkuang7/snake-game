e625f1cf31c9963fa614b4da59f5197cceb26f5f
Daniel Kuang <danielkuang7@github.com>
2020-05-26 09:13:36 -0400

    Created snake

--- snake.js
+++ snake.js
@@ -31,10 +31,68 @@ let food = {
 
 let score = 0;
 
+// control the snake
+
+let d;
+
+document.addEventListener("keydown", direction);
+
+function direction(event){
+    if(event.keyCode == 37){
+        d = "LEFT";
+    } else if (event.keyCode == 38){
+        d = "UP";
+    } else if (event.keyCode == 39){
+        d = "RIGHT";
+    } else if (event.keyCode == 40){
+        d = "DOWN";
+    }
+}
+
 //  draw everything to the canvas
 
 function draw(){
     ctx.drawImage(ground,0,0);
+
+    for( let i = 0; i < snake.length; i++){
+        ctx.fillStyle = ( i==0 )? "green" : "white";
+        ctx.fillRect(snake[i].x, snake[i].y,box,box);
+
+        ctx.fillStyle = "red";
+        ctx.fillRect(snake[i].x, snake[i].y,box,box);
+    }
+
+    ctx.drawImage(foodImg, food.x, food.y);
+
+    // old head position
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    // remove the tail
+    snake.pop();
+
+    // which direction
+    if( d == "LEFT") snakeX -= box;
+    if( d == "UP") snakeY -= box;
+    if( d == "RIGHT") snakeX += box;
+    if( d == "DOWN") snakeY += box;
+
+    // remove the tail
+    snake.pop();
+
+    // add new Head
+
+    let newHead = {
+        x : snakeX,
+        y : snakeY
+    }
+
+    snake.unshift(newHead);
+
+
+    ctx.fillStyle = "white";
+    ctx.font = "45px Changa one";
+    ctx.fillText(score, 2*box, 1.6*box);
 }
 
 // call draw function every 100 ms
